aec6d01b51cf9ed8bf9662bd0ad33de86a9eb837
cristobalwiechert <cristobalw@gmail.com>
2025-07-31 18:25:29 -0400

    upload the code

---
+++ osrslib/__init__.py
@@ -0,0 +1,15 @@
+from .osrs import (
+    RegionHSV,
+    Recorder,
+    click,
+    get_mouse_coordinates,
+    wait_for_image
+)
+
+__all__ = [
+    "RegionHSV",
+    "Recorder",
+    "click",
+    "get_mouse_coordinates",
+    "wait_for_image"
+]
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='osrs_utils',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=requirements,
+    description='Funciones útiles para programar en OSRS',
+    author='Cristobal Wiechert',
+    author_email='tu_correo@example.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
